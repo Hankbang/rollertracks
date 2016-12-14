ee4c0b9ae45cf95a2c32c5f811310d1d6f8f6a59
gc2hnik <gc2hnik@AP710303>
2016-12-14 17:31:31 +0100

    winforms checklistbox

---
+++ WinFormsCheckBoxList/WinFormsCheckBoxList/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsCheckBoxList
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, Tuple<int, List<string>>> content = new Dictionary<string, Tuple<int, List<string>>>
+        {
+            { "Pečivo", new Tuple<int, List<string>>(0 , new List<string> { "Rohlík", "Chleba"}) },
+            { "Zelenina", new Tuple<int, List<string>>(1, new List<string> { "Cibule", "Česnek"}) }
+        };
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            int buttonTop = 5;
+            int index = 0;
+            foreach (var item in content.OrderBy(x => x.Value.Item1))
+            {
+                this.checkedListBox1.Items.Add(item.Key, false);                
+                Button b = new Button();
+                b.Click += ItemButton_Click;
+                b.Parent = this;
+                b.Text = item.Key;
+                b.Left = 150;
+                b.Top = buttonTop;
+                b.Tag = item.Key;
+                buttonTop += b.Height;
+                index++;
+            }
+        }
+
+        internal void SetChecked(string key)
+        {
+            checkedListBox1.SetItemChecked(content[key].Item1, true);
+        }
+
+        private void ItemButton_Click(object sender, EventArgs e)
+        {
+            string key = (string)((Button)sender).Tag;
+            Form2 form2 = new Form2(key, content[key].Item2, this);
+            form2.Show();
+
+        }
+    }
+}
---
+++ WinFormsCheckBoxList/WinFormsCheckBoxList/Form2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsCheckBoxList
+{
+    public partial class Form2 : Form
+    {
+        private Form1 form1;
+        private string key;
+        private Button b;
+        public Form2(string key, List<string> items, Form1 form1)
+        {
+            InitializeComponent();
+
+            this.form1 = form1;
+            this.key = key;
+
+            foreach (var item in items)
+            {
+                checkedListBox1.Items.Add(item);
+            }
+
+            checkedListBox1.MouseUp += CheckedListBox1_MouseUp;
+
+            b = new Button();
+            b.Text = "Ok";
+            b.Click += Ok_Click;
+            b.Top = 150;
+            b.Left = 50;
+            b.Parent = this;
+            b.Enabled = false;    
+        }
+
+        private void CheckedListBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            b.Enabled = checkedListBox1.CheckedItems.Count == checkedListBox1.Items.Count;
+        }
+
+        private void Ok_Click(object sender, EventArgs e)
+        {
+            form1.SetChecked(this.key);
+            this.Close();
+        }
+    }
+}
